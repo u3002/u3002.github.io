e4d7e270b09a58eb3dfc72932a8aa5b7baaa31dd
u3002 <20873340+u3002@users.noreply.github.com>
2020-12-09 04:41:25 +0800

    .

--- js/bg2.js
+++ js/bg2.js
@@ -1,53 +1,53 @@
 ! function () {
-    //·â×°·½·¨£¬Ñ¹ËõÖ®ºó¼õÉÙÎÄ¼ş´óĞ¡
+    //å°è£…æ–¹æ³•ï¼Œå‹ç¼©ä¹‹åå‡å°‘æ–‡ä»¶å¤§å°
     function get_attribute(node, attr, default_value) {
         return node.getAttribute(attr) || default_value;
     }
-    //·â×°·½·¨£¬Ñ¹ËõÖ®ºó¼õÉÙÎÄ¼ş´óĞ¡
+    //å°è£…æ–¹æ³•ï¼Œå‹ç¼©ä¹‹åå‡å°‘æ–‡ä»¶å¤§å°
     function get_by_tagname(name) {
         return document.getElementsByTagName(name);
     }
-    //»ñÈ¡ÅäÖÃ²ÎÊı
+    //è·å–é…ç½®å‚æ•°
     function get_config_option() {
         var scripts = get_by_tagname("script"),
           script_len = scripts.length,
-          script = scripts[script_len - 1]; //µ±Ç°¼ÓÔØµÄscript
+          script = scripts[script_len - 1]; //å½“å‰åŠ è½½çš„script
         return {
-            l: script_len, //³¤¶È£¬ÓÃÓÚÉú³ÉidÓÃ
+            l: script_len, //é•¿åº¦ï¼Œç”¨äºç”Ÿæˆidç”¨
             z: get_attribute(script, "zIndex", -1), //z-index
             o: get_attribute(script, "opacity", 0.975), //opacity
             c: get_attribute(script, "color", "0,31,86"), //color"0,31,86"
             n: get_attribute(script, "count", 99) //count
         };
     }
-    //ÉèÖÃcanvasµÄ¸ß¿í
+    //è®¾ç½®canvasçš„é«˜å®½
     function set_canvas_size() {
         canvas_width = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
         canvas_height = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     }
 
-    //»æÖÆ¹ı³Ì
+    //ç»˜åˆ¶è¿‡ç¨‹
     function draw_canvas() {
         context.clearRect(0, 0, canvas_width, canvas_height);
-        //Ëæ»úµÄÏßÌõºÍµ±Ç°Î»ÖÃÁªºÏÊı×é
-        var e, i, d, x_dist, y_dist, dist; //ÁÙÊ±½Úµã
-        //±éÀú´¦ÀíÃ¿Ò»¸öµã
+        //éšæœºçš„çº¿æ¡å’Œå½“å‰ä½ç½®è”åˆæ•°ç»„
+        var e, i, d, x_dist, y_dist, dist; //ä¸´æ—¶èŠ‚ç‚¹
+        //éå†å¤„ç†æ¯ä¸€ä¸ªç‚¹
         random_points.forEach(function (r, idx) {
             r.x += r.xa,
-            r.y += r.ya, //ÒÆ¶¯
+            r.y += r.ya, //ç§»åŠ¨
             r.xa *= r.x > canvas_width || r.x < 0 ? -1 : 1,
-            r.ya *= r.y > canvas_height || r.y < 0 ? -1 : 1, //Åöµ½±ß½ç£¬·´Ïò·´µ¯
-            context.fillRect(r.x - 0.5, r.y - 0.5, 1, 1); //»æÖÆÒ»¸ö¿í¸ßÎª1µÄµã
-            //´ÓÏÂÒ»¸öµã¿ªÊ¼
+            r.ya *= r.y > canvas_height || r.y < 0 ? -1 : 1, //ç¢°åˆ°è¾¹ç•Œï¼Œåå‘åå¼¹
+            context.fillRect(r.x - 0.5, r.y - 0.5, 1, 1); //ç»˜åˆ¶ä¸€ä¸ªå®½é«˜ä¸º1çš„ç‚¹
+            //ä»ä¸‹ä¸€ä¸ªç‚¹å¼€å§‹
             for (i = idx + 1; i < all_array.length; i++) {
                 e = all_array[i];
-                // µ±Ç°µã´æÔÚ
+                // å½“å‰ç‚¹å­˜åœ¨
                 if (null !== e.x && null !== e.y) {
-                    x_dist = r.x - e.x; //xÖá¾àÀë l
-                    y_dist = r.y - e.y; //yÖá¾àÀë n
-                    dist = x_dist * x_dist + y_dist * y_dist; //×Ü¾àÀë, m
+                    x_dist = r.x - e.x; //xè½´è·ç¦» l
+                    y_dist = r.y - e.y; //yè½´è·ç¦» n
+                    dist = x_dist * x_dist + y_dist * y_dist; //æ€»è·ç¦», m
 
-                    dist < e.max && (e === current_point && dist >= e.max / 2 && (r.x -= 0.03 * x_dist, r.y -= 0.03 * y_dist), //¿¿½üµÄÊ±ºò¼ÓËÙ
+                    dist < e.max && (e === current_point && dist >= e.max / 2 && (r.x -= 0.03 * x_dist, r.y -= 0.03 * y_dist), //é è¿‘çš„æ—¶å€™åŠ é€Ÿ
                       d = (e.max - dist) / e.max,
                       context.beginPath(),
                       context.lineWidth = d / 2,
@@ -59,28 +59,28 @@
             }
         }), frame_func(draw_canvas);
     }
-    //´´½¨»­²¼£¬²¢Ìí¼Óµ½bodyÖĞ
-    var the_canvas = document.createElement("canvas"), //»­²¼
-      config = get_config_option(), //ÅäÖÃ
+    //åˆ›å»ºç”»å¸ƒï¼Œå¹¶æ·»åŠ åˆ°bodyä¸­
+    var the_canvas = document.createElement("canvas"), //ç”»å¸ƒ
+      config = get_config_option(), //é…ç½®
       canvas_id = "c_n" + config.l, //canvas id
       context = the_canvas.getContext("2d"), canvas_width, canvas_height,
       frame_func = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (func) {
           window.setTimeout(func, 1000 / 45);
       }, random = Math.random,
       current_point = {
-          x: null, //µ±Ç°Êó±êx
-          y: null, //µ±Ç°Êó±êy
-          max: 20000 // È¦°ë¾¶µÄÆ½·½
+          x: null, //å½“å‰é¼ æ ‡x
+          y: null, //å½“å‰é¼ æ ‡y
+          max: 20000 // åœˆåŠå¾„çš„å¹³æ–¹
       },
       all_array;
     the_canvas.id = canvas_id;
     the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:" + config.z + ";opacity:" + config.o;
     get_by_tagname("body")[0].appendChild(the_canvas);
 
-    //³õÊ¼»¯»­²¼´óĞ¡
+    //åˆå§‹åŒ–ç”»å¸ƒå¤§å°
     set_canvas_size();
     window.onresize = set_canvas_size;
-    //µ±Ê±Êó±êÎ»ÖÃ´æ´¢£¬Àë¿ªµÄÊ±ºò£¬ÊÍ·Åµ±Ç°Î»ÖÃĞÅÏ¢
+    //å½“æ—¶é¼ æ ‡ä½ç½®å­˜å‚¨ï¼Œç¦»å¼€çš„æ—¶å€™ï¼Œé‡Šæ”¾å½“å‰ä½ç½®ä¿¡æ¯
     window.onmousemove = function (e) {
         e = e || window.event;
         current_point.x = e.clientX;
@@ -89,23 +89,23 @@
         current_point.x = null;
         current_point.y = null;
     };
-    //Ëæ»úÉú³Éconfig.nÌõÏßÎ»ÖÃĞÅÏ¢
+    //éšæœºç”Ÿæˆconfig.næ¡çº¿ä½ç½®ä¿¡æ¯
     for (var random_points = [], i = 0; config.n > i; i++) {
-        var x = random() * canvas_width, //Ëæ»úÎ»ÖÃ
+        var x = random() * canvas_width, //éšæœºä½ç½®
           y = random() * canvas_height,
-          xa = 2 * random() - 1, //Ëæ»úÔË¶¯·½Ïò
+          xa = 2 * random() - 1, //éšæœºè¿åŠ¨æ–¹å‘
           ya = 2 * random() - 1;
-        // Ëæ»úµã
+        // éšæœºç‚¹
         random_points.push({
             x: x,
             y: y,
             xa: xa,
             ya: ya,
-            max: 6000 //Õ´¸½¾àÀë
+            max: 6000 //æ²¾é™„è·ç¦»
         });
     }
     all_array = random_points.concat([current_point]);
-    //0.1Ãëºó»æÖÆ
+    //0.1ç§’åç»˜åˆ¶
     setTimeout(function () {
         draw_canvas();
     }, 100);
