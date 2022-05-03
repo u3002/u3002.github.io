a9dbe1e7a5ec36e4b99c2c1f88a0fb9d694009e5
u3002 <20873340+u3002@users.noreply.github.com>
2022-05-04 07:06:52 +0800

    .

--- js/Snow.js
+++ js/Snow.js
@@ -1,22 +1,22 @@
 Particle3D=function(material){
 	THREE.Particle.call(this,material);
 	this.velocity=new THREE.Vector3(0,0,0);//速度;
-	this.velocity.rotateX(10);//旋转;
+	this.velocity.rotateX(10);//旋轉;
 	this.gravity=new THREE.Vector3(0,0,0.5);//加速度;
-	this.drag=1;//速度相乘系数;
+	this.drag=1;//速度相乘系數;
 };
 //Particle:粒子;
 //prototype:原形;
 Particle3D.prototype=new THREE.Particle();
-Particle3D.prototype.constructor=Particle3D;//构造函数
+Particle3D.prototype.constructor=Particle3D;//構造函數
 Particle3D.prototype.updatePhysics=function(){
-	this.velocity.multiplyScalar(this.drag);//矢量相乘函数
-	this.velocity.addSelf(this.gravity);//矢量相加函数
-	this.position.addSelf(this.velocity);//矢量相加函数
+	this.velocity.multiplyScalar(this.drag);//矢量相乘函數
+	this.velocity.addSelf(this.gravity);//矢量相加函數
+	this.position.addSelf(this.velocity);//矢量相加函數
 }
-var TO_RADIANS=Math.PI/180;//角度向弧度转换系数*
+var TO_RADIANS=Math.PI/180;//角度向弧度轉換系數*
 THREE.Vector3.prototype.rotateY=function(angle){
-	//绕Y轴顺时针旋转angle;
+	//繞Y軸順時針旋轉angle;
 	cosRY=Math.cos(angle*TO_RADIANS);
 	sinRY=Math.sin(angle*TO_RADIANS);
 	var tempz=this.z;
@@ -25,7 +25,7 @@ THREE.Vector3.prototype.rotateY=function(angle){
 	this.z=(tempx*-sinRY)+(tempz*cosRY);
 }
 THREE.Vector3.prototype.rotateX=function(angle){
-	//绕X轴顺时针旋转angle;
+	//繞X軸順時針旋轉angle;
 	cosRY=Math.cos(angle*TO_RADIANS);
 	sinRY=Math.sin(angle*TO_RADIANS);
 	var tempz=this.z;;
@@ -34,7 +34,7 @@ THREE.Vector3.prototype.rotateX=function(angle){
 	this.z=(tempy*-sinRY)+(tempz*cosRY);
 }
 THREE.Vector3.prototype.rotateZ=function(angle){
-	//绕Z轴顺时针旋转angle;
+	//繞Z軸順時針旋轉angle;
 	cosRY=Math.cos(angle*TO_RADIANS);
 	sinRY=Math.sin(angle*TO_RADIANS);
 	var tempx=this.x;;
